546eb199de05b84214ba115ac12ff54616140058
orc-yy <whleewhlee1004@naver.com>
2025-07-19 15:24:57 +0900

    Player Controller

---
+++ Assets/Scripts/Player Controller.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rigid;
+
+    [SerializeField]
+    private float moveSpeed;
+    
+    void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector2 moveDirection = new Vector2(x, y).normalized;
+        rigid.linearVelocity = moveDirection * moveSpeed;
+    }
+}
