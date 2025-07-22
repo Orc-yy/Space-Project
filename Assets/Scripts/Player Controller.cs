5949dea2d6c601de7ab42c5aad38aafaa3c0ec09
orc-yy <whleewhlee1004@naver.com>
2025-07-22 21:46:13 +0900

    플레이어 점화 크기 조절 & 적 판정 조정

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -4,31 +4,51 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private Rigidbody2D rigid;
+
+    public Transform chlidObject;
+
     [SerializeField]
     private float moveSpeed;
     public GameObject explosionEffectPrefab;
     public GameObject bulletPrefab;
     public Transform bulletSpawnPoint;
+
+
+
     public float bulletFireDelay;
     public float playerHealth;
     private float playerCurrentHealth;
+
+
+
     void Start()
     {
         rigid = gameObject.GetComponent<Rigidbody2D>();
         playerCurrentHealth = playerHealth;
         StartCoroutine(AutoFireBullet());
     }
+
     void Update()
     {
         Move();
     }
+
     private void Move()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
+        if(Input.GetAxisRaw("Vertical") < 0)
+        {
+            chlidObject.transform.localScale = new Vector3(1, 1, 2.5f);
+        }
+        else if(Input.GetAxisRaw("Vertical") > 0)
+        {
+            chlidObject.transform.localScale = new Vector3(2, 2, 2.5f);
+        }
         Vector2 moveDirection = new Vector2(x, y).normalized;
         rigid.linearVelocity = moveDirection * moveSpeed;
     }
+
     public void TakeDamage(float damage)
     {
         playerCurrentHealth -= damage;
